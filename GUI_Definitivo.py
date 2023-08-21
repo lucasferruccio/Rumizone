fce2bda3d441a04fb491965164a0b78bb2154f70
Lucas Paiva <104527748+lucasferruccio@users.noreply.github.com>
2023-08-21 20:07:24 -0300

    Update GUI_Definitivo.py

--- GUI_Definitivo.py
+++ GUI_Definitivo.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from funcApp import *
 
 verde_fundo = "#172526"
+verde_botao = "#3d9079"
 branco = "#FFFFFF"
 preto = "#000000"
 verde = "#20CD8D"
@@ -85,6 +86,7 @@ class rumizone(CTk):
     #Frame do menu de acesso a todos as funções do aplicativo
 
     def menu(self):
+        #Fecha o frame passado
         self.destruir_frames()
 
         #Informações do Frame do menu
@@ -94,35 +96,66 @@ class rumizone(CTk):
         #Logo Rumizone
         custom_font_title = CTkFont(family="Roboto", size=40, weight="bold")
         self.rumizone_centro = CTkLabel(master=self.root, text="Rumizone", width=0, fg_color=verde_fundo, font=custom_font_title)
-        self.rumizone_centro.place(x=500, y=40)
+        self.rumizone_centro.place(x=480, y=40)
 
         #Botão para o Frame da Conta
 
-        self.btconta = CTkButton(master=self.root, image=caixa_conta,text="",fg_color=verde_fundo, width= 100,height= 70)
+        self.btconta = CTkButton(master=self.root, image=btn_conta,text="",fg_color=verde_fundo, width= 100,height= 70)
         self.btconta.place(x=300,y=100)
 
         #Botão para o Frame dos animais
-        self.btboi = CTkButton(master=self.root, image=caixa_vaca,text="", fg_color=verde_fundo, width= 100,height= 70)
+        self.btboi = CTkButton(master=self.root, image=btn_animais,text="", fg_color=verde_fundo, width= 100,height= 70, command=self.btn_animais)
         self.btboi.place(x=650,y=100)
         self.btboi["font"] = ("Verdana", "15")
 
         #Botão para o Frame de Registros
-        self.btmonitoramento = CTkButton(master=self.root, image=caixa_monitoramento,text="", fg_color=verde_fundo, width= 100,height= 70)
+        self.btmonitoramento = CTkButton(master=self.root, image=btn_monitoramento,text="", fg_color=verde_fundo, width= 100,height= 70)
         self.btmonitoramento.place(x=300,y=380)
 
         #Botão para o Frame de Análise Comportamental
-        self.btrendimento = CTkButton(master=self.root, image=caixa_rendimento,text="", fg_color=verde_fundo, width= 100,height= 70)
+        self.btrendimento = CTkButton(master=self.root, image=btn_registros,text="", fg_color=verde_fundo, width= 100,height= 70)
         self.btrendimento.place(x=650,y=380)
 
+    #Animais:
+
+    #Botão de acesso ao Frame de animais.
+    def btn_animais(self):
+        self.animais()
+
+    def animais(self):
+        #Fecha o frame passado
+        self.destruir_frames()
+
+        #Informações do Frame dos animais
+        self.menu = CTkFrame(master=self.root, width=1200, height=700, fg_color=verde_fundo)
+        self.menu.pack()
+
+        #Coleta de alguns dados para a criação dos botões, para retirar a quantidade e os ids das vacas no DB
+        
+        ids_vacas = [id[0] for id in select("id_vaca", "vacas")]
+        #Botão bonito:
+        ''' 
+        self.
+
+        #Criação de botões iguais aos numéros de ids(único para cada animal)
+        for contagem_vacas in range(len(ids_vacas)):
+            #Coleta de dados básicos para a aparição no botão
+            info_vaca_botao = botao_vaca(ids_vacas[contagem_vacas])
+            #Coleta de todos os dados das vacas
+            info_vaca_completa = info_vaca(info_vaca_botao[0])
+            #Criação dos botões
+            self.btn_vaca = CTkButton(master=self.root, text=("teste"), image=vaca_img, compound="top", width=200, height=200, fg_color=verde_botao)
+            self.btn_vaca.grid() '''
 
 #Carregamento das imagens:
 path = os.path.dirname(__file__)
 path_barras = path.replace('\\', '/')
 pathFinal = path_barras + "/imagens"
-caixa_conta = CTkImage(Image.open(pathFinal + "/caixa conta.png"), size=(200,200))
-caixa_vaca = CTkImage(Image.open(pathFinal + "/caixa_boi.png"), size=(200,200))
-caixa_monitoramento = CTkImage(Image.open(pathFinal + "/caixa monitoramento.png"), size=(200,200))
-caixa_rendimento = CTkImage(Image.open(pathFinal + "/caixa_rendimento.png"), size=(200,200))
+btn_conta = CTkImage(Image.open(pathFinal + "/caixa conta.png"), size=(200,200))
+btn_animais = CTkImage(Image.open(pathFinal + "/caixa_boi.png"), size=(200,200))
+btn_monitoramento = CTkImage(Image.open(pathFinal + "/caixa monitoramento.png"), size=(200,200))
+btn_registros = CTkImage(Image.open(pathFinal + "/caixa_rendimento.png"), size=(200,200))
+vaca_img = CTkImage(Image.open(pathFinal + "/vaca.png"), size=(100,100))
     
         
 #Iniciação do sistema, segundo Thiago é bom manter ele assim.
